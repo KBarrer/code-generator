109b4593ddf699cba2a4a85eace941d0f20d1efe
KSB <kyle.barrer@gmail.com>
2021-04-10 14:34:57 -0600

    fully functioning

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,14 @@
-// Assignment code here
-function options() {
+var getRandomLower = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+  
+var getRandomUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+  
+var getRandomNumber = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+ 
+var getRandomSymbol = [" ", "!", "#", "$", "%", "&", "(", ")", "*", "+", "-", ".", "/", "<", "=", ">", "?", "@", "[", "]", "^", "_", "{", "|", "}"]
+
+var possibleChar = [];
+
+function generatePassword() {
   var pwdLen = Number(prompt("How many characters would you like your password to be?"));
   while (isNaN(pwdLen) || pwdLen < 8 || pwdLen > 128) pwdLen = Number(prompt("Length must be 8-128 characters. How many characters would you like your password to be?"));
 
@@ -16,26 +25,114 @@ function options() {
     symbol = confirm("Would you like to use special characters?");
   }
 
-  var pwdChars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*?";
-  var randPassword = Array(pwdLen).fill(pwdChars).map(function(x) {return x[Math.floor(Math.random() * x.length)]}).join('');
-  console.log(randPassword);
+  if (alphaLower) {
+    possibleChar += (getRandomLower.join(""));
+  }
+  // do all of the options!!!!!
+  if (alphaUpper) {
+    possibleChar += (getRandomUpper.join(""));
+  }
+  
+  if (number) {
+    possibleChar += (getRandomNumber.join(""));
+  }
+  
+  if (symbol) {
+    possibleChar += (getRandomSymbol.join(""));
+  }
+
+  var passwordOptions = {
+    pwdLen: pwdLen,
+    alphaUpper: alphaUpper,
+    alphaLower: alphaLower,
+    number: number,
+    symbol: symbol
+  };
+
+  var options = passwordOptions;
+
+for (var i = 0; i < pwdLen; i++){
+
+  var password = possibleChar;
 
 };
+
+
+
+  var randPassword = Array(pwdLen).fill(possibleChar).map(function(x) {return x[Math.floor(Math.random() * x.length)]}).join('');
+
+
+  var passwordText = document.querySelector("#password");
+    
+  passwordText.value = randPassword;
   
-
-
+  return;
+};
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  
-  passwordText.value = password;
-
-}
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", options);
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", generatePassword);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function generatePassword() {
+
+
+// // 2 for loops 
+// // one goes over all options together
+
+// for (i = 0; i < pwdLen; i++) {
+
+// }
+
+// //   // one mix in guarantee characters
+
+// //   // return result .join
+
+// };
+
+// console.log(generatePassword());
+
+
+
